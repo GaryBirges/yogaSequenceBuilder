8002472218a99ca7c22a2d58073b17051d6c9a73
GaryBirges <gary.birges@marketaccess.com>
2020-01-03 16:10:42 +0000

    "save" works (no db)

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DragulaService } from 'ng2-dragula';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-builder',
@@ -7,11 +8,22 @@ import { DragulaService } from 'ng2-dragula';
   styleUrls: ['./builder.component.css']
 })
 export class BuilderComponent implements OnInit {
-images=[]
-  constructor(private dragula: DragulaService) { }
+  images=[]
+  originals=[]
+  rows=[]
+  target=[]
+  sequenceName
+  sequence =[]
+
+  subs = new Subscription();
+  constructor(private dragula: DragulaService) {
+
+  }
 
   ngOnInit() {
     this.images = new Array(45)
+    // this.originals = new Array(45)
+    this.rows = new Array(1)
     let dragula=this.dragula.createGroup('originals', {
       removeOnSpill: (target) => {
         return target.id !== 'orig';
@@ -21,10 +33,25 @@ images=[]
         console.log(source.id === 'orig')
         return source.id === 'orig';
       },
+      copyItem:(item) => ({ ...item }),
       accepts: (el, target, source, sibling) => {
         return target.id !== 'orig';
       }
     })
+    this.dragula.drop('originals').subscribe((x)=>{
+      // setTimeout(() => {
+        (console.log(x))
+      // }, 1000);
+    })
+  }
+  addRow(){
+    this.rows.push(this.rows.length + 1)
+  }
+
+  save(name){
+    console.log(this.sequence)
+    console.log(this.images)
+   console.log( name)
   }
 
 }
